2b6745046712b815e3f31d265622d2c0496ecd4d
jack-williams1 <32389253+jack-williams1@users.noreply.github.com>
2022-07-01 14:47:06 +1200

    Add files via upload

--- Generate_MATfile.m
+++ Generate_MATfile.m
@@ -4,12 +4,12 @@
 clear
 close all
 
-[num_sec,txt_sec,raw_sec] = xlsread('MSSD_sourcecalcs/MSSD.xlsx',2);
+[num_sec,txt_sec,raw_sec] = xlsread('MSSM_sourcecalcs/MSSM.xlsx',2);
 
-[num_fault,txt_fault,raw_fault] = xlsread('MSSD_sourcecalcs/MSSD.xlsx',3);
+[num_fault,txt_fault,raw_fault] = xlsread('MSSM_sourcecalcs/MSSM.xlsx',3);
 
-[num_multi_fault,txt_multi_fault,raw_multi_fault] = xlsread('MSSD_sourcecalcs/MSSD.xlsx',4);
+[num_multi_fault,txt_multi_fault,raw_multi_fault] = xlsread('MSSM_sourcecalcs/MSSM.xlsx',4);
 
-[num_MSSD,txt_MSSD,raw_MSSD] = xlsread('MSSD_sourcecalcs/MSSD.xlsx',5);
+[num_MSSM,txt_MSSM,raw_MSSM] = xlsread('MSSM_sourcecalcs/MSSM.xlsx',5);
 %Create mat file
-save('MSSD_sources')
+save('MSSM_sources')
